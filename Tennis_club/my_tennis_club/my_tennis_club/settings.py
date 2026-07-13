1879e7bf8ed6e7a2a6d562d9ad911fcd08e1d27c
Denis Deus <deusdenis40@gmail.com>
2026-07-13 12:44:46 +0300

    Add testing view and template; update URLs and enable debug mode

--- Tennis_club/my_tennis_club/my_tennis_club/settings.py
+++ Tennis_club/my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7pd^&$yxs78g^(d&$35gsz-ky!1dtfv+4&3nyt!vr15d(4t7-n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
