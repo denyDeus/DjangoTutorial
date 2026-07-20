a55fd85735490e3a43845d713b7f79e8f3dbecae
Denis Deus <deusdenis40@gmail.com>
2026-07-20 18:38:47 +0300

    Refactor member view and template to display member names; update context variable

--- Tennis_club/my_tennis_club/members/views.py
+++ Tennis_club/my_tennis_club/members/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Member
 
@@ -23,8 +23,9 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
+  mymembers = Member.objects.all().values()
   template = loader.get_template('template.html')
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],   
+    'mymembers': 'mymembers',   
   }
   return HttpResponse(template.render(context, request))
